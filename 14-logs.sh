7cf04331ccc4fd2e405b2ee3348c3355a11099f2
SamaArun <s.arunkumargoud8@gmail.com>
2026-02-01 21:09:48 +0530

    modified

--- 14-logs.sh
+++ 14-logs.sh
@@ -10,23 +10,23 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FLODER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FLODER
-echo "Script started executing at: $(date)" &>>$LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R ERROR:: Please run this script with root access $N" &>>$LOG_FILE
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
 
     exit 1
 else
-    echo "You are running with root access" &>>$LOG_FILE
+    echo "You are running with root access" | tee -a $LOG_FILE
 
 fi
 # validate function takes input as exit status, what commad they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]; then
-        echo -e "Installing $2 is ... $G SUCCESS $N" &>>$LOG_FILE
+        echo -e "Installing $2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
 
     else
-        echo -e "Installing $2 is ... $R FAILURE $N" &>>$LOG_FILE
+        echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
 
         exit 1
     fi
@@ -39,15 +39,16 @@ dnf list installed $PACKAGE &>>$LOG_FILE
 #check already installed or not, if installed $? is 0, then
 #if not installed is not 0 .expression is true
 if [ $? -ne 0 ]; then
-    echo "MariaDB is not installed.. going to install it" &>>$LOG_FILE
+    echo "MariaDB is not installed.. going to install it" | tee -a $LOG_FILE
 
 
     dnf install $PACKAGE -y &>>$LOG_FILE
 
+
     VALIDATE $? "mariadb105-server"
     
 else
-    echo -e " $Y MariaDB is already installed $N .. nothing to do" &>>$LOG_FILE
+    echo -e " $Y MariaDB is already installed $N .. nothing to do" | tee -a $LOG_FILE
 
 fi
 
@@ -58,7 +59,7 @@ dnf list installed $PACKAGE &>>$LOG_FILE
 # check already installed or not, if installed $? is 0
 # if not installed $? is not 0, expression is true
 if [ $? -ne 0 ]; then
-    echo "Python3 is not installed.. going to install it" &>>$LOG_FILE
+    echo "Python3 is not installed.. going to install it" | tee -a $LOG_FILE
 
 
     dnf install $PACKAGE -y &>>$LOG_FILE
@@ -79,10 +80,22 @@ if [ $? -ne 0 ]; then
     echo "Nginx is not installed.. going to install it" &>>$LOG_FILE
 
 
-    dnf install $PACKAGE -y &>>$LOG_FILE
+    dnf install $PACKAGE &>>$LOG_FILE
+
 
     VALIDATE $? "nginx"
 else
-    echo -e "nothing to do..$Y nginx is already installed $N" &>>$LOG_FILE
+    echo -e "nothing to do..$Y nginx is already installed $N" | tee -a $LOG_FILE
 
 fi
+
+
+
+
+
+#tee -a $LOG_FILE will show to user and store in log file
+
+#&>>$LOG_FILE only will store in logfile, will not show on screen
+
+# sudo sh 14-logs.sh 
+# [ec2-user@ip-172-31-18-115 Shell-Practice]$ cat /var/log/shellscript-logs/14-logs.log 
