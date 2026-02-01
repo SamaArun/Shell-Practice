6a8f06099c5d561f22e3edb96eb4edfadcbc4861
SamaArun <s.arunkumargoud8@gmail.com>
2026-02-01 16:50:09 +0530

    modified

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-USERID=$(id-u)
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
 else
-    echo "You are running wiyh root access"
+    echo "You are running with root access"
 fi
 
 dnf install mysql -y
