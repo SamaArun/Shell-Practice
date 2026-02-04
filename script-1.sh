19b0b85dc9c9c9849f1772d09cde2aacf2de221b
SamaArun <s.arunkumargoud8@gmail.com>
2026-02-04 23:04:44 +0530

    modified

--- script-1.sh
+++ script-1.sh
@@ -4,4 +4,25 @@ GREETING="Hello, Good Morning"
 echo "$GREETING"
 echo "PID of SCRIPT-1: $$"
 
-./script-2.sh
+#./script-2.sh
+
+source ./script-2.sh
+
+
+
+
+
+
+
+
+
+
+# 🔢 Permission numbers (octal values)
+
+# Each permission has a number:
+
+# Permission	Symbol	Number
+# Read	r	4
+# Write	w	2
+# Execute	x	1
+# No access	–	0
