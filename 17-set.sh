c3ddecbcfc39d7950c33f53c5aee26938e36cd2e
SamaArun <s.arunkumargoud8@gmail.com>
2026-02-05 20:33:40 +0530

    modified

---
+++ 17-set.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+set -e #if any error,exits
+
+echo "Hi, Good Morning"
+echoooo "Hello, this will be error"
+echo "Hello, Morning"
+
+
+#without set -e command, the script will go for next line if error is there
